4504e3e2e679b6224a9ed9175390df9625976d9a
filippbudko <filipp.budko.dev@gmail.com>
2022-03-14 11:31:26 +0300

    first commit

---
+++ src/pages/app.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { AppProps } from 'next/app';
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+    return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ src/server/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { RenderModule } from 'nest-next';
+import Next from 'next';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [RenderModule.forRootAsync(Next({ dev: true }), { viewsDir: null })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
